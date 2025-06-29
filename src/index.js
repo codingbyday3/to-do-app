b278cc2fb2938148fc1defb663e238fd377f3d29
Klemen <klemen.bussines12@gmail.com>
2025-06-29 17:09:36 +0200

    Style: projects page

--- src/index.js
+++ src/index.js
@@ -2,22 +2,22 @@ import "./styles.css"
 import { generateHomePage } from "./home"
 import { generateCalendar } from "./calendar"
 
-function renderContent(){
+// function renderContent(){
 
-  const homeBtn = document.querySelector(".btn-home")
-  const calendarBtn = document.querySelector(".btn-calendar")
-  const projectsBtn = document.querySelector(".btn-projects")
+//   const homeBtn = document.querySelector(".btn-home")
+//   const calendarBtn = document.querySelector(".btn-calendar")
+//   const projectsBtn = document.querySelector(".btn-projects")
 
 
-  generateHomePage()
-  homeBtn.addEventListener("click", ()=>{
-    generateHomePage()
-  })
+//   generateHomePage()
+//   homeBtn.addEventListener("click", ()=>{
+//     generateHomePage()
+//   })
 
-  calendarBtn.addEventListener("click",()=>{
-    generateCalendar()
+//   calendarBtn.addEventListener("click",()=>{
+//     generateCalendar()
 
-  })
-}
+//   })
+// }
 
-renderContent()
+// renderContent()
